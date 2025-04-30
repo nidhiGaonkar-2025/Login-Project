94ec834cad06d187c7616384b73a61e56766fd74
nidhiGaonkar-2025 <gaonkarnidhi2004@gmail.com>
2025-04-30 23:37:37 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const pasth = require("path");
+const bcrypt = require("bcrypt");
+const collection = require("./config");
+
+const app = express();
+//convert data into json format
+app.use(express.json());
+
+app.use(express.urlencoded({extended: false}));
+
+//use EJS as the  view engine
+app.set('view engine', 'ejs');
+//static file
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("login");
+});
+
+app.get("/signup", (req,res) => {
+    res.render("signup");
+});
+
+//Register User
+app.post("/signup", async (req, res) => {
+    
+    const data = {
+        name: req.body.username,
+        password: req.body.password
+    }
+
+    //check if the user already exists in the database
+    const existingUser = await collection.findOne({name: data.name});
+    if(existingUser){
+        res.send("User already exists. Please choose a different username.");
+    }else {
+        // hash the password using  bcrypt 
+        const saltRounds = 10;//No. of salt rounds for bcrypt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+        data.password = hashedPassword;//Replace the hash password with original pswd
+
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+    }
+
+});
+
+//Login user
+app.post("/login",  async (req, res) => {
+    try {
+        const  check = await collection.findOne({name: req.body.username});
+        if(!check) {
+            
+            res.send("user name cannot found")
+        }
+
+        //compare  the has password from database with the plain text
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+        if(isPasswordMatch) {
+            res.render("home");
+        }else {
+            res.send("wrong password")
+        }
+    }catch{
+        res.send("Wrong Details");
+
+    }
+});
+
+
+const port = 4002;
+app.listen(port, () => {
+    console.log(`Server running on Port: ${port} `);
+})
